fc88e5538f5149e9302b10614f78297ffd320585
Caio P <caiopontes1707@hotmail.com>
2023-08-12 20:47:51 -0300

    Start Project

---
+++ Course/Entities/Enum/WorkerLevel.cs
@@ -0,0 +1,8 @@
+﻿namespace Course.Entities.Enum;
+
+public enum WorkerLevel : int
+{ 
+    Junior = 0,
+    MidLevel = 1,
+    Senior = 2
+}
---
+++ Course/Entities/HourContract.cs
@@ -0,0 +1,24 @@
+﻿namespace Course.Entities;
+
+public class HourContract
+{
+    public DateTime Data { get; set; }
+    public double ValuePerHour { get; set; }
+    public int Hours { get; set; }
+
+    public HourContract ()
+    {
+    }
+
+    public HourContract(DateTime date, double valuePerHour, int hours)
+    {
+        Data = date;
+        ValuePerHour = valuePerHour;
+        Hours = hours;
+    }
+
+    public double TotalValue ()
+    {
+        return Hours * ValuePerHour;
+    }
+}
